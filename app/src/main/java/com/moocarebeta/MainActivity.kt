75a31958332c68e0a22c794063710c85b5ca9342
RayhanHafidh <rayhanhafidh@gmail.com>
2023-02-05 19:54:28 +0700

    commit tanggal 5

--- app/src/main/java/com/moocarebeta/MainActivity.kt
+++ app/src/main/java/com/moocarebeta/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.moocarebeta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startItButton: Button = findViewById(R.id.button2)
+
+        startItButton.setOnClickListener{
+            val intent = Intent(this, Welcome::class.java)
+            startActivity(intent)
+        }
     }
 }
